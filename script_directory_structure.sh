9e0fc3dc726f9f567f641835556d971565ce4567
rajkumarroy007 <rajkumarroy007@yahoo.com>
2024-08-21 18:40:12 +0000

    Create a Directory Structure

---
+++ script_directory_structure.sh
@@ -0,0 +1,8 @@
+sudo mkdir /home/user
+sudo mkdir /home/user/project
+sudo mkdir /home/user/project/project1
+sudo mkdir /home/user/project/project2
+sudo mkdir /home/user/project/project3
+sudo mkdir /home/user/documents
+sudo mkdir /home/user/downloads
+exit
